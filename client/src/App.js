ed95ddf99d27ad70424fe857f1bd707fe25af4f1
Your NameMichelle <watts.e.michelle@gmail.com>
2023-07-25 10:47:55 +1000

    render be url

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   // Fetching message from backend on mount
   useEffect(() => {
-    fetch("http://localhost:4000")
+    fetch("https://mern-deploy-o7jn.onrender.com")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
